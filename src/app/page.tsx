1f7d64befc08ff795b3e693c7e1abc53eb640698
Riosmena <emi.riosmena@gmail.com>
2024-08-22 21:16:12 -0600

    chore(home): Add basic prview of home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
 const Home = () => (
   <div>
-    <h1>José Riosmena</h1>
-    <p>Buenas :)</p>
+    <section>
+      <h1>José Riosmena</h1>
+      <p>Buenas :)</p>
+    </section>
+
+    <section>
+      <h2>About</h2>
+      <p>
+        I am a software engineer from Mexico. I love to learn new things and
+        share knowledge with others.
+      </p>
+    </section>
+
+    <section>
+      <h2>Projects</h2>
+      <ul>
+        <li>
+          <a href="/projects/1">Project 1</a>
+        </li>
+        <li>
+          <a href="/projects/2">Project 2</a>
+        </li>
+        <li>
+          <a href="/projects/3">Project 3</a>
+        </li>
+      </ul>
+    </section>
+
+    <section>
+      <h2>Contact</h2>
+      <p>You can reach me at: </p>
+    </section>
   </div>
 );
 
